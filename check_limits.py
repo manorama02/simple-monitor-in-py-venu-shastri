bb493db8ab1086fbfa0d2033fbcac6dd6ad5d619
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-09 12:58:38 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,27 +1,30 @@
 def print_to_console(parameter):
   print("{} is out of range!".format(parameter))#not testable
   
-def isvalid_temperature(temp):
+def isvalid_temperature(temp,consoleprint):
   if temp > 0 or temp < 45:
     return True
+  consoleprint('temperature')
   return False
 
-def isvalid_soc(soc):
+def isvalid_soc(soc,consoleprint):
   if soc > 20 or soc < 80:
     return True
+  consoleprint('soc')
   return False
 
-def isvalid_charge_rate(cr):
+def isvalid_charge_rate(cr,consoleprint):
   if cr < 0.8:
     return True
+  consoleprint('charge rate')
   return False
   
-def battery_is_ok(temperature, soc, charge_rate):
-  t_cond=isvalid_temperature(temperature)
-  s_cond=isvalid_soc(soc)
-  cr_cond=isvalid_charge_rate(charge_rate)
+def battery_is_ok(temperature, soc, charge_rate,consoleprint):
+  t_cond=isvalid_temperature(temperature,consoleprint)
+  s_cond=isvalid_soc(soc,consoleprint)
+  cr_cond=isvalid_charge_rate(charge_rate,consoleprint)
   return t_cond & s_cond & cr_cond
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(25, 70, 0.7,print_to_console) is True)
+  assert(battery_is_ok(50, 85, 0,,print_to_console) is False)
